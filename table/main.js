86725b12dad7094989757b85d9a12632d5b6f113
Tanyy1028 <tanmaycpatil1028@gmail.com>
2025-04-25 10:26:18 +0530

    Add files via upload

---
+++ table/main.js
@@ -0,0 +1,28 @@
+// wap to create a function to generate table of user given number.
+
+// let x =  5;  
+// let x =(prompt("enter the value"))
+// function table(x){
+//     for(let i = 1; i <= 10 ; i++){
+//         document.writeln(x +"*"+i + " = " + (x * i));
+//         document.writeln("<br>")
+//     }
+// }
+// table(x);
+
+
+// let n=(prompt("enter the value"))
+// function table(n){
+//     for(let i = 1; i <= n ; i++){
+//     for (let j = 1; j <= n; j++) {
+//             document.writeln(i + ' x ' + i + ' = ' + (j * i));
+//             document.writeln("<br>")
+//         }
+//     }
+// }
+// table(n);
+
+
+
+
+  
